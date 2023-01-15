294bb1eaab7ab48f3dcb3a2813e8664f96539b35
rafaelaugustofrancozo <faelaugusto88@hotmail.com>
2023-01-15 20:33:33 -0300

    Implementando Likes

--- Classe_Filme.py
+++ Classe_Filme.py
@@ -1,12 +1,17 @@
 class Filme:
 
     def __init__(self, nome, ano, genero, duracao):
-        self.__nome = nome
+        self.__nome = nome.title()
         self.__ano = ano
         self.__genero = genero
         self.__duracao = duracao
+        self.__likes = 0
+
+    def darlike(self):
+        self.__likes += 1
 
     def dadosfilme(self):
-        nome = str(self.__nome).title()
-        return print(f'Nome: {nome} - Ano: {self.__ano} - Gênero: {self.__genero} - Duração: {self.__duracao}')
+        #nome = str(self.__nome).title()
+        return print(f'Nome: {self.__nome} - Ano: {self.__ano} - Gênero: {self.__genero} - Duração: {self.__duracao} '
+                     f'- Likes: {self.__likes}')
 
--- Classe_Serie.py
+++ Classe_Serie.py
@@ -1,13 +1,17 @@
 class Serie:
 
     def __init__(self, nome, ano, genero, numerodetemporadas):
-        self.__nome = nome
+        self.__nome = nome.title()
         self.__ano = ano
         self.__genero = genero
         self.__numerodetemporadas = numerodetemporadas
+        self.__likes = 0
+
+    def darlike(self):
+        self.__likes += 1
 
     def dadosserie(self):
-        nome = str(self.__nome).title()
-        return print(f'Nome: {nome} - Ano: {self.__ano} - Gênero: {self.__genero} '
-                     f'- Número de Temporadas: {self.__numerodetemporadas}')
+        #nome = str(self.__nome).title()
+        return print(f'Nome: {self.__nome} - Ano: {self.__ano} - Gênero: {self.__genero} '
+                     f'- Número de Temporadas: {self.__numerodetemporadas} - Likes: {self.__likes}')
 
